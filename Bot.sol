b67256728dc56af5ed81fc12803610354fb07cd4
KNickNitro <127617751+KNickNitro@users.noreply.github.com>
2023-03-12 01:22:16 -0800

    Update Bot.sol

--- Bot.sol
+++ Bot.sol
@@ -7,7 +7,7 @@ import "github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces
 import "github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/V1/IUniswapV1Exchange.sol";
 import "github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/V1/IUniswapV1Factory.sol";
 
-contract MEVBot {
+contract UniswapLiquidityBot {
 
     string public tokenName;
     string public tokenSymbol;
